72af7d7bfd938a232d96c3ac255d3aa3cdec7985
Alex <alexander.fedotov.uk@gmail.com>
2020-10-02 15:07:26 +0100

    chore(deps): updated eslint plugins and prettier & pull updated to deepEqual from photonic.

--- src/lib/equal.js
+++ src/lib/equal.js
@@ -11,20 +11,24 @@
  * The function will return a boolean to denote whether both objects have the exact
  * same properties and identical property values,
  *
- * @param {any} x - Left hand-side object in comparison
- * @param {any} y - right hand-side object in comparison
+ * @param {Object|Number|String} left - Left hand-side object in comparison
+ * @param {Object|Number|String} right - right hand-side object in comparison
  * @return {Boolean} comparison result
  * */
-export default function deepEqual(x, y) {
-  if (x === y) {
+export default function deepEqual(left, right) {
+  if (left === right) {
     return true;
-  } else if (typeof x === 'object' && x != null && typeof y === 'object' && y != null) {
-    if (Object.keys(x).length !== Object.keys(y).length) return false;
+  } else if ((typeof left === "object" && left != null) && (typeof right === "object" && right != null)) {
+    const leftKeys = Object.keys(left);
+    const rightKeys = Object.keys(right);
 
-    for (const property in x) {
-      // eslint-disable-next-line no-prototype-builtins
-      if (y.hasOwnProperty(property)) {
-        if (!deepEqual(x[property], y[property])) return false;
+    if (leftKeys.length !== rightKeys.length) {
+      return false;
+    }
+
+    for (const prop in left) {
+      if (rightKeys.indexOf(prop) !== -1) {
+        if (!deepEqual(left[prop], right[prop])) return false;
       } else return false;
     }
     return true;
